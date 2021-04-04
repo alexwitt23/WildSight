ad25917dbf8cef8b7ebe4921944fb2a9f4471346
Abhinav Raghunathan <abhinavr2121@gmail.com>
2021-04-04 13:50:08 -0500

    fixing navbar, adding boostrap.js

--- wild_sight/web/src/layouts/Main.vue
+++ wild_sight/web/src/layouts/Main.vue
@@ -1,8 +1,11 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <v-link class="nav-link active" aria-current="page" href="/">Home</v-link>
         </li>
@@ -12,6 +15,7 @@
         <li class="nav-item">
           <v-link class="nav-link" href="/demo">Demo</v-link>
         </li>
+
       </ul>
     </div>
   </div>
